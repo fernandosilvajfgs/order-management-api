ce91425829b96ed5508755e36a20e2b6f38617fb
Fernando Silva <fernandosilvajfgs@gmail.com>
2024-12-23 12:22:38 +0000

    models small fix

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,7 +1,7 @@
 public class Item
 {
     public int Id { get; set; }
-    public string Descricao { get; set; }
+    public string Descricao { get; set; } = string.Empty;
     public decimal PrecoUnitario { get; set; }
     public int Qtd { get; set; }
 }
